098628353fa121392a62c1b576bc2fa91b0aa245
clark <panf2333@gmail.com>
2025-02-04 22:22:37 +0800

    fix SIM105
    Signed-off-by: clark <panf2333@gmail.com>

--- vllm/scripts.py
+++ vllm/scripts.py
@@ -1,5 +1,6 @@
 # The CLI entrypoint to vLLM.
 import argparse
+import contextlib
 import os
 import signal
 import sys
@@ -44,11 +45,8 @@ def serve(args: argparse.Namespace) -> None:
 
 
 def connect(args: argparse.Namespace) -> None:
-    try:
+    with contextlib.suppress(KeyboardInterrupt):
         uvloop.run(run_disagg_connector(args))
-    except KeyboardInterrupt:
-        pass
-
 
 
 def interactive_cli(args: argparse.Namespace) -> None:
